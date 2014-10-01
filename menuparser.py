b68185aeb74f04a524372c967fe7f8d3313c5ffd
Tom Jaspers <tomjaspers@gmail.com>
2014-10-01 11:38:13 +0200

    Add parsing if the menu name contains no text, but only an image. This should only happen on Thursdays for "Veggiedag".

--- menuparser.py
+++ menuparser.py
@@ -44,6 +44,11 @@ LOCAL_MONTHS['oktober'] = 10
 LOCAL_MONTHS['november'] = 11
 LOCAL_MONTHS['december'] = 12
 
+def is_veggiedag_img(img):
+    src = img.get('src', None) if img is not None else None
+    if src is None:
+        return False
+    return "veggiedag" in src
 
 def normalize_text(text):
     return text.replace(u'\xa0', u' ').strip()
@@ -56,6 +61,7 @@ def parse_restaurant(name, url):
     sel_day_divs = CSSSelector('#content .views-row')
     sel_date_span = CSSSelector('.date-display-single')
     sel_tablerows = CSSSelector('table tr')
+    sel_img = CSSSelector('img')
     
     # Request and build the DOM Tree
     r = requests.get(url)
@@ -99,7 +105,11 @@ def parse_restaurant(name, url):
             if menu_color is None:
                 logging.warning(name + " - Failed to get a color for the menu: '" + menu_name + "' (" + str(date) + ")")
                 menu_color = '#ffffff'
-                
+            # Sometimes there is no menu name, but just an image (e.g., for "Veggiedag")
+            if not menu_name:
+                img = sel_img(tds[0])
+                img = img[0] if img else None
+                menu_naam = 'Veggiedag' if is_veggiedag_img(img) else 'Menu'
             menus.append({'name': menu_name, 'dish': menu_dish, 'color': menu_color})
         data.append({'date': str(date), 'menus': menus})
     return data
