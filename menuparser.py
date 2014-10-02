92dcc00bf1946da42bd50b1201081e764b248a13
Tom Jaspers <tomjaspers@gmail.com>
2014-10-02 14:54:23 +0200

    Format code according to conventions (remove trailing whitespace etc.)

--- menuparser.py
+++ menuparser.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import io
 import os
 import json
@@ -10,24 +9,24 @@ import lxml.html
 from lxml.cssselect import CSSSelector
 
 # Path where the JSONs will get written. Permissions are your job.
-SAVE_PATH = '/Users/tjs/Development/vubresto-server/' 
+SAVE_PATH = '/Users/tjs/Development/vubresto-server/'
 
 # Urls of the pages that will get parsed
 URL_ETTERBEEK = 'https://my.vub.ac.be/resto/etterbeek'
 URL_JETTE = 'https://my.vub.ac.be/resto/jette'
 
-# Mapping of colors for the menus. 
+# Mapping of colors for the menus.
 COLOR_MAPPING = {}
-COLOR_MAPPING['soep']      = '#fdb85b' # yellow
-COLOR_MAPPING['menu 1']    = '#68b6f3' # blue
-COLOR_MAPPING['dag menu']  = '#68b6f3' #blue
-COLOR_MAPPING['dagmenu']   = '#68b6f3' #blue
-COLOR_MAPPING['health']    = '#ff9861' # orange
-COLOR_MAPPING['menu 2']    = '#cc93d5' # purple
-COLOR_MAPPING['veggie']    = '#87b164' # green
-COLOR_MAPPING['pasta']     = '#de694a' # red
+COLOR_MAPPING['soep'] = '#fdb85b' # yellow
+COLOR_MAPPING['menu 1'] = '#68b6f3' # blue
+COLOR_MAPPING['dag menu'] = '#68b6f3' #blue
+COLOR_MAPPING['dagmenu'] = '#68b6f3' #blue
+COLOR_MAPPING['health'] = '#ff9861' # orange
+COLOR_MAPPING['menu 2'] = '#cc93d5' # purple
+COLOR_MAPPING['veggie'] = '#87b164' # green
+COLOR_MAPPING['pasta'] = '#de694a' # red
 COLOR_MAPPING['pasta bar'] = '#de694a' # red
-COLOR_MAPPING['wok']       = '#6c4c42' # brown
+COLOR_MAPPING['wok'] = '#6c4c42' # brown
 
 # Months in Dutch, to allow the parsing of the (Dutch) site
 LOCAL_MONTHS = {}
@@ -48,25 +47,25 @@ def is_veggiedag_img(img):
     src = img.get('src', None) if img is not None else None
     if src is None:
         return False
+    print src
     return "veggiedag" in src
 
 def normalize_text(text):
     return text.replace(u'\xa0', u' ').strip()
 
-
 def parse_restaurant(name, url):
     data = []
-    
+
     # Construct CSS Selectors
     sel_day_divs = CSSSelector('#content .views-row')
     sel_date_span = CSSSelector('.date-display-single')
     sel_tablerows = CSSSelector('table tr')
     sel_img = CSSSelector('img')
-    
+
     # Request and build the DOM Tree
     r = requests.get(url)
     tree = lxml.html.fromstring(r.text)
-    
+
     # Apply selector to get divs representing 1 day
     day_divs = sel_day_divs(tree)
     for day_div in day_divs:
@@ -82,7 +81,7 @@ def parse_restaurant(name, url):
                                  month, # month
                                  int(date_components[0])) # day
         else:
-            # If we couldn't find a month, we'll just try to use the previous date and hope that one was better
+            # If we couldn't find a month, we'll just try to use the previous date
             logging.warning(name + " - Failed to get a month for the month_name: " + month_name)
             try:
                 prev_date_components = map(int, data[len(data) - 1]['date'].split('-'))
@@ -103,36 +102,32 @@ def parse_restaurant(name, url):
             menu_dish = normalize_text(tds[1].text_content())
             menu_color = COLOR_MAPPING.get(menu_name.lower(), None)
             if menu_color is None:
-                logging.warning(name + " - Failed to get a color for the menu: '" + menu_name + "' (" + str(date) + ")")
+                logging.warning(name + " - Failed to get a color for the menu: '" +\
+                                menu_name + "' (" + str(date) + ")")
                 menu_color = '#ffffff'
             # Sometimes there is no menu name, but just an image (e.g., for "Veggiedag")
             if not menu_name:
                 img = sel_img(tds[0])
                 img = img[0] if img else None
-                menu_naam = 'Veggiedag' if is_veggiedag_img(img) else 'Menu'
+                menu_name = 'Veggiedag' if is_veggiedag_img(img) else 'Menu'
             menus.append({'name': menu_name, 'dish': menu_dish, 'color': menu_color})
         data.append({'date': str(date), 'menus': menus})
     return data
 
-
-
 def write_to_json(data, filename):
     with io.open(os.path.join(SAVE_PATH, filename), 'w', encoding='utf8') as f:
         f.write(unicode(json.dumps(data, ensure_ascii=False)))
-        
+
 def parse_and_save(name, url):
     try:
         data = parse_restaurant(name, url)
     except Exception:
         logging.exception(name + " - Failed to parse")
         data = []
-        
     try:
         write_to_json(data, name.lower() + '.json')
     except Exception:
         logging.exception(name + " - Failed to save to json")
-
-
 
 def main():
     # Configure the logger
